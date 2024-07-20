906776eaff8dc39db071b57e1c11956f54eb4038
sachinnegi <negi.sachin1906@gmail.com>
2024-07-20 18:19:50 +0530

    Data ingestion

---
+++ src/components/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+from src.components.data_transformation import DataTransformation
+# from src.components.data_transformation import DataTransformationConfig
+
+# from src.components.model_trainer import ModelTrainerConfig
+# from src.components.model_trainer import ModelTrainer
