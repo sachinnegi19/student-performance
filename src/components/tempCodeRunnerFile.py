7e37528c6d6493b9eb81e29af5c1a364cdd05176
sachinnegi <negi.sachin1906@gmail.com>
2024-07-21 16:08:47 +0530

    Model trainer

--- src/components/tempCodeRunnerFile.py
+++ src/components/tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-from src.components.data_transformation import DataTransformation
-# from src.components.data_transformation import DataTransformationConfig
-
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+src
